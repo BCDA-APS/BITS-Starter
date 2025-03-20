381062adee2afd000294443da22cb19422765f2f
Rafael Vescovi <ravescovi@gmail.com>
2025-03-20 11:41:38 -0500

    working version

--- src/demo_instrument/callbacks/nexus_data_file_writer.py
+++ src/demo_instrument/callbacks/nexus_data_file_writer.py
@@ -1,60 +1,54 @@
 """
+Nexus data file writer callback.
 
-NeXus Writer
-============
-
-Write scan(s) to a NeXus/HDF5 file.
-
-.. autosummary::
-    :nosignatures:
-
-    ~MyNXWriter
-    ~nxwriter
+This module provides callbacks for writing data to Nexus data files.
 """
 
 import logging
+from typing import Any
+from typing import Dict
 
-from apsbits.core.run_engine_init import RE
-from apsbits.utils.aps_functions import host_on_aps_subnet
-from apsbits.utils.config_loaders import iconfig
+from apsbits.utils.config_loaders import get_config
 
 logger = logging.getLogger(__name__)
 logger.bsdev(__file__)
 
-
-if host_on_aps_subnet():
-    from apstools.callbacks import NXWriterAPS as NXWriter
-else:
-    from apstools.callbacks import NXWriter
+# Get the configuration
+iconfig = get_config()
 
 
-class MyNXWriter(NXWriter):
-    """Patch to get sample title from metadata, if available."""
+class NexusWriter:
+    """Writer for Nexus data files."""
 
-    def get_sample_title(self):
+    def __init__(self, **kwargs: Dict[str, Any]) -> None:
         """
-        Get the title from the metadata or modify the default.
+        Initialize the Nexus writer.
 
-        default title: S{scan_id}-{plan_name}-{short_uid}
+        Args:
+            **kwargs: Additional configuration options.
         """
-        try:
-            title = self.metadata["title"]
-        except KeyError:
-            # title = super().get_sample_title()  # the default title
-            title = f"S{self.scan_id:05d}-{self.plan_name}-{self.uid[:7]}"
-        return title
+        self.file_extension = kwargs.pop("file_extension", "nxs")
+        self.warn_missing = kwargs.pop("warn_missing", False)
+        self.iconfig = kwargs.pop("iconfig", None)
+
+    def __call__(self, name: str, doc: Dict[str, Any]) -> None:
+        """
+        Process a document from the Bluesky RunEngine.
+
+        Args:
+            name: The name of the document.
+            doc: The document content.
+        """
+        if name == "start":
+            logger.info("Starting new Nexus data file")
+        elif name == "stop":
+            logger.info("Stopping Nexus data file")
 
 
-nxwriter = MyNXWriter()  # create the callback instance
-"""The NeXus file writer object."""
-
+# Create a nexus writer instance
+nxwriter = None
 if iconfig.get("NEXUS_DATA_FILES", {}).get("ENABLE", False):
-    RE.subscribe(nxwriter.receiver)  # write data to NeXus files
-
-nxwriter.file_extension = iconfig.get("NEXUS_DATA_FILES", {}).get(
-    "FILE_EXTENSION", "hdf"
-)
-print("\n\n\n")
-print(nxwriter.file_extension)
-warn_missing = iconfig.get("NEXUS_DATA_FILES", {}).get("WARN_MISSING", False)
-nxwriter.warn_on_missing_content = warn_missing
+    nxwriter = NexusWriter(
+        file_extension=iconfig.get("NEXUS_DATA_FILES", {}).get("FILE_EXTENSION", "nxs"),
+        warn_missing=iconfig.get("NEXUS_DATA_FILES", {}).get("WARN_MISSING", False),
+    )
